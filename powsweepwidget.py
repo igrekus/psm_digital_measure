9188de289d8cb6c9dd71da066e470adca2f5de38
igrekus <upload234@mail.ru>
2020-01-27 16:03:03 +0300

    WIP: implement plotting logic

--- powsweepwidget.py
+++ powsweepwidget.py
@@ -15,6 +15,22 @@ class PowSweepWidget(QWidget):
         self._plot = PlotWidget(parent=None, toolbar=True)
         self._ui.verticalLayout.addWidget(self._plot)
 
-    @pyqtSlot
+        self._init()
+
+    def _init(self):
+        self._plot.set_title('Прогон по мощности')
+        self._plot.set_xlabel('F, GHz', labelpad=-2)
+        self._plot.set_ylabel('Pow, dB', labelpad=-2)
+        self._plot.set_xlim(4, 8)
+        # self._plot.set_ylim(pars['ylim'][0], pars['ylim'][1])
+        self._plot.grid(b=True, which='major', color='0.5', linestyle='-')
+        self._plot.tight_layout()
+
+    @pyqtSlot()
     def on_btnPowSweep_clicked(self):
-        self._controller.pow_sweep()
+        freqs, amps = self._controller.pow_sweep()
+        self._plot.clear()
+        self._init()
+
+        self._plot.plot(freqs, amps)
+
