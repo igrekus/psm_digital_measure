bec4646dcaef66f2e136623639c9bd66fa6bef32
igrekus <upload234@mail.ru>
2020-02-11 14:15:15 +0300

    WIP: repplace matplotlib in the plotting subsystem with the PyQtChart

--- primaryplotwidget.py
+++ primaryplotwidget.py
@@ -1,128 +1,168 @@
-import errno
-import itertools
-import os
+from PyQt5 import uic
+from PyQt5.QtChart import QChartView, QLineSeries, QValueAxis
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QPointF, Qt
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QWidget
 
-from PyQt5.QtWidgets import QGridLayout, QWidget
-from mytools.plotwidget import PlotWidget
+
+class PlotWidget(QChartView):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.setRenderHint(QPainter.Antialiasing)
+
+        self._chart = self.chart()
+        self._axis_x = QValueAxis()
+        self._axis_y = QValueAxis()
+
+        self.series = QLineSeries(self)
+
+        self._chart.addSeries(self.series)
+        self._chart.addAxis(self._axis_x, Qt.AlignBottom)
+        self._chart.addAxis(self._axis_y, Qt.AlignLeft)
+
+        self.series.attachAxis(self._axis_x)
+        self.series.attachAxis(self._axis_y)
+
+        self._axis_x.setTickCount(5)
+        self._axis_x.setRange(0, 10)
+
+        self._axis_y.setTickCount(5)
+        self._axis_y.setRange(0, 10)
+
+        self._chart.legend().hide()
+
+    def plot(self, xs, ys):
+        self.series.replace([QPointF(x, y) for x, y in zip(xs, ys)])
+
+    @property
+    def axes_titles(self):
+        return self._axis_x.titleText(), self._axis_y.titleText()
+
+    @axes_titles.setter
+    def axes_titles(self, value):
+        x, y = value
+        self._axis_x.setTitleText(x)
+        self._axis_y.setTitleText(y)
+
+    @property
+    def title(self):
+        return self._chart.title()
+
+    @title.setter
+    def title(self, value):
+        self._chart.setTitle(value)
+
+    @property
+    def legend(self):
+        return self._chart.legend()
 
 
 class PrimaryPlotWidget(QWidget):
 
-    params = {
-        0: {
-            '11': {
-                'title': 'SWR in',
-                'xlabel': 'F, GHz',
-                'xlim': [1, 1.6],
-                'ylabel': 'loss, dB',
-                'ylim': []
-            },
-            '12': {
-                'title': 'SWR out',
-                'xlabel': 'F, GHz',
-                'xlim': [1, 1.6],
-                'ylabel': 'loss, dB',
-                'ylim': []
-            },
-            '21': {
-                'title': f'Phase',
-                'xlabel': 'F, GHz',
-                'xlim': [1, 1.6],
-                'ylabel': 'phase, deg',
-                'ylim': []
-            },
-            '22': {
-                'title': 'Phase error',
-                'xlabel': 'F, GHz',
-                'xlim': [1, 1.6],
-                'ylabel': 'Phase error, deg',
-                'ylim': []
-            }
-        },
-    }
+    startMeasure = pyqtSignal()
+    exportResult = pyqtSignal()
 
     def __init__(self, parent=None, result=None):
-        super().__init__(parent)
+        super().__init__(parent=parent)
 
+        self._ui = uic.loadUi('primaryplotwidget.ui', self)
         self._result = result
 
-        self._grid = QGridLayout()
+        self._plotFreq = PlotWidget(self)
+        self._ui.plotGrid.addWidget(self._plotFreq, 0, 0)
+        self._plotFreq.axes_titles = 'Ctrl voltage, V', 'Freq, MHz'
 
-        self._plot11 = PlotWidget(parent=None, toolbar=True)
-        self._plot12 = PlotWidget(parent=None, toolbar=True)
-        self._plot21 = PlotWidget(parent=None, toolbar=True)
-        self._plot22 = PlotWidget(parent=None, toolbar=True)
+        self._plotKvco = PlotWidget(self)
+        self._ui.plotGrid.addWidget(self._plotKvco, 0, 1)
+        self._plotKvco.axes_titles = 'Ctrl voltage, V', 'Kvco, MHz/V'
 
-        self._grid.addWidget(self._plot11, 0, 0)
-        self._grid.addWidget(self._plot12, 0, 1)
-        self._grid.addWidget(self._plot21, 1, 0)
-        self._grid.addWidget(self._plot22, 1, 1)
+        self._plotSupply1Current = PlotWidget(self)
+        self._ui.plotGrid.addWidget(self._plotSupply1Current, 1, 0)
+        self._plotSupply1Current.axes_titles = 'Ctrl voltage, V', 'Src current, mA'
 
-        self.setLayout(self._grid)
+        self._plotPower = PlotWidget(self)
+        self._ui.plotGrid.addWidget(self._plotPower, 1, 1)
+        self._plotPower.axes_titles = 'Ctrl voltage, V', 'Pow, dBm'
 
-        self._init()
+        self._plotPushing = PlotWidget(self)
+        self._ui.plotGrid.addWidget(self._plotPushing, 2, 0)
+        self._plotPushing.axes_titles = 'Ctrl voltage, V', 'Pushing, Mhz/V'
 
-    def _init(self, dev_id=0):
+        self._ready = False
 
-        def setup_plot(plot, pars: dict):
-            # plot.set_tight_layout(True)
-            plot.subplots_adjust(bottom=0.150)
-            plot.set_title(pars['title'])
-            plot.set_xlabel(pars['xlabel'], labelpad=-2)
-            plot.set_ylabel(pars['ylabel'], labelpad=-2)
-            plot.set_xlim(pars['xlim'][0], pars['xlim'][1])
-            # plot.set_ylim(pars['ylim'][0], pars['ylim'][1])
-            plot.grid(b=True, which='major', color='0.5', linestyle='-')
-            plot.tight_layout()
+    def plotResult(self, result):
+        self._plotFreq.plot(xs=result.tune_voltage, ys=result.frequency)
+        self._plotKvco.plot(xs=result.tune_voltage, ys=result.kvco)
+        self._plotSupply1Current.plot(xs=result.tune_voltage, ys=result.supply_current)
+        self._plotPower.plot(xs=result.tune_voltage, ys=result.power)
+        self._plotPushing.plot(xs=result.tune_voltage, ys=result.pushing)
+        self._plotPhaseNoise.plot(xs=result.tune_voltage, ys=result.noise)
 
-        setup_plot(self._plot11, self.params[dev_id]['11'])
-        setup_plot(self._plot12, self.params[dev_id]['12'])
-        setup_plot(self._plot21, self.params[dev_id]['21'])
-        setup_plot(self._plot22, self.params[dev_id]['22'])
+    # event handlers
+    @pyqtSlot()
+    def on_btnMeasure_clicked(self):
+        self.startMeasure.emit()
 
-    def clear(self):
-        self._plot11.clear()
-        self._plot12.clear()
-        self._plot21.clear()
-        self._plot22.clear()
+    @pyqtSlot()
+    def on_btnExport_clicked(self):
+        self.exportResult.emit()
 
-    def plot(self, dev_id=0):
-        self.clear()
-        self._init(dev_id)
-        self._result.process()
+    @pyqtSlot(bool)
+    def on_checkV1_toggled(self, value):
+        self._params.v_sup1_on = value
 
-        print('plotting primary stats')
+    @pyqtSlot(bool)
+    def on_checkV2_toggled(self, value):
+        self._params.v_sup2_on = value
 
-        swr_out = self._result.swr_out
-        swr_in = self._result.swr_in
-        phase = self._result.phase
-        phase_err = self._result.phase_err
+    @pyqtSlot(float)
+    def on_spinV1_valueChanged(self, value):
+        self._params.v_sup1 = value
 
-        freqs = [f / 1_000_000_000 for f in self._result.freqs]
+    @pyqtSlot(float)
+    def on_spinV2_valueChanged(self, value):
+        self._params.v_sup2 = value
 
-        for sw_in, sw_out, ph, ph_e in zip(swr_in, swr_out, phase, phase_err):
-            self._plot11.plot(freqs, sw_in)
-            self._plot12.plot(freqs, sw_out)
-            self._plot21.plot(freqs, ph)
-            self._plot22.plot(freqs, ph_e)
+    @pyqtSlot(float)
+    def on_spinDutVstart_valueChanged(self, value):
+        self._params.v_dut_start = value
 
-        # for xs, ys in zip(self._domain.errorPerCodeXs, self._domain.errorPerCodeYs):
-        #     self._plot12.plot(xs, ys)
-        #
-        # for xs, ys in zip(self._domain.inputInverseLossXs, self._domain.inputInverseLossYs):
-        #     self._plot21.plot(xs, ys)
-        #
-        # for xs, ys in zip(self._domain.outputInverseLossXs, self._domain.outputInverseLossYs):
-        #     self._plot22.plot(xs, ys)
+    @pyqtSlot(float)
+    def on_spinDutVend_valueChanged(self, value):
+        self._params.v_dut_end = value
 
-    def save(self, img_path='./image'):
-        try:
-            os.makedirs(img_path)
-        except OSError as ex:
-            if ex.errno != errno.EEXIST:
-                raise IOError('Error creating image dir.')
+    @pyqtSlot(float)
+    def on_spinDutVstep_valueChanged(self, value):
+        self._params.v_dut_step = value
 
-        for plot, name in zip([self._plot11, self._plot12, self._plot21, self._plot22], ['stats.png', 'cutoff.png', 'delta.png', 'double-triple.png']):
-            plot.savefig(img_path + name, dpi=400)
+    @pyqtSlot(float)
+    def on_spinOffsetF1_valueChanged(self, value):
+        self._params.f_offset1 = value * 1_000
 
+    @pyqtSlot(float)
+    def on_spinOffsetF2_valueChanged(self, value):
+        self._params.f_offset2 = value * 1_000
 
+    @pyqtSlot(float)
+    def on_spinOffsetF3_valueChanged(self, value):
+        self._params.f_offset3 = value * 1_000
+
+    @pyqtSlot(float)
+    def on_spinOffsetF4_valueChanged(self, value):
+        self._params.f_offset4 = value * 1_000
+
+    # props
+    @property
+    def ready(self):
+        return self._ready
+
+    @ready.setter
+    def ready(self, value):
+        self._ui.btnMeasure.setEnabled(value)
+        self._ready = value
+
+    @property
+    def params(self):
+        return self._params
