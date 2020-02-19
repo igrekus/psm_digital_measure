5f6d2208d86d0fb4d7b9b978180e9466e7c888d9
igrekus <upload234@mail.ru>
2020-02-19 18:08:26 +0300

    FIX: implement phase unwrapper

--- measureresult.py
+++ measureresult.py
@@ -4,6 +4,19 @@ import statistics
 import numpy as np
 
 
+def unwrap(xw):
+    dist = 180
+    xu = list(xw)
+    for i in range(1, len(xw)):
+        diff = xw[i] - xw[i - 1]
+        if diff > dist:
+            for j in range(i, len(xu)):
+                xu[j] -= 2 * dist
+        elif diff < -dist:
+            for j in range(i, len(xu)):
+                xu[j] += 2 * dist
+    return xu
+
 
 def calc_vswr(in_mags: list):
     temp = map(lambda x: x / 20, in_mags)
@@ -102,6 +115,7 @@ class MeasureResult:
 
     def _calc_phase_err(self):
         self._s21s_ph = [np.unwrap(s, discont=np.rad2deg(np.pi)) for s in self._s21s_ph]
+        self._s21s_ph = [unwrap(s) for s in self._s21s_ph]
         ph0 = self._s21s_ph[0]
         self._s21s_ph_err = [calc_phase_error(s, ph0, ideal) for s, ideal in zip(self._s21s_ph[1:], self._ideal_phase[1:])]
 
