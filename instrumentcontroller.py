b46c4b053a3dfc21fe664da3acf5092dc3bc6cb3
igrekus <upload234@mail.ru>
2020-06-02 15:24:25 +0300

    REF: move devug dataset to a git-ignored folder

--- instrumentcontroller.py
+++ instrumentcontroller.py
@@ -169,7 +169,7 @@ class InstrumentController(QObject):
             # with open(f's2p_{code}.s2p', mode='wt', encoding='utf-8') as f:
             #     f.write(res)
             if mock_enabled:
-                with open(f's2p_{code}.s2p', mode='rt', encoding='utf-8') as f:
+                with open(f'ref/sample_data/s2p_{code}.s2p', mode='rt', encoding='utf-8') as f:
                     res = list(f.readlines())[0].strip()
             out.append(parse_float_list(res))
 
